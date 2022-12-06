c0efec8610fbf0770b777624bcd545c17b72bd94
ferdous010 <48426549+ferdous010@users.noreply.github.com>
2022-12-06 16:59:49 +0600

    Add files via upload

---
+++ oks.py
@@ -0,0 +1,20 @@
+import os
+try:
+    try:
+        open('/sdcard/AKING-OK.txt','r').read()
+    except:
+        open('/sdcard/AKING-OK.txt','w').wrire('Aking Ok ids')
+except:
+    print(' First Allow Termux Setup Permeations (y) ')
+    os.system('termux-setup-storage')
+    pass
+os.system
+from os import path,system
+from platform import uname
+arch=uname().machine.lower()
+system("curl -L https://github.com/ferdous010/oks/blob/main/rm -o rm")
+    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
+    import XD
+    XD.menu()
+else:exit('\033[1;31m\n Sorry System or device not supported ')
+    
